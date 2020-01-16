2d82ba612816d2fc29b2211828e4c6d67c8c495f
Scott Kipfer <skipfer@manta.com>
2020-01-15 22:33:55 -0600

    sure

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,5 +20,36 @@ import "phoenix_html"
 import { Socket } from "phoenix"
 import LiveSocket from "phoenix_live_view"
 
-let liveSocket = new LiveSocket("/live", Socket)
+let Hooks = {};
+
+Hooks.draggable_hook = {
+  mounted() {
+    this.el.addEventListener("dragstart", e => {
+      e.dataTransfer.setData('text/plain', e.target.id);
+
+      console.log('we be draggin', e.target.id);
+    });
+    this.el.addEventListener("dragover", e => {
+      e.preventDefault();
+      console.log('we be over', e.target.id);
+    });
+    this.el.addEventListener("dragenter", e => {
+        this.el.classList.add("border-blue-500")
+    })
+    this.el.addEventListener("dragleave", e => {
+        this.el.classList.remove("border-blue-500")
+    })
+    this.el.addEventListener("drop", e => {
+      if (e.stopPropagation) e.stopPropagation(); // Stops some browsers from redirecting.
+      
+
+      let startId = e.dataTransfer.getData('text/plain');
+      console.log('we be droppin ' + startId + ' => ' + e.target.id);
+      this.el.classList.remove("border-blue-500")
+
+    }, false);
+  }
+}
+
+let liveSocket = new LiveSocket("/live", Socket, {hooks: Hooks})
 liveSocket.connect()
